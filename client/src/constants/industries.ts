1ca684f441abc2dfc6b8aa2aca7654e60dfbcf68
Greg Frasco <frascog17@gmail.com>
2023-03-09 15:15:47 -0500

    feat: add all industries

--- client/src/constants/industries.ts
+++ client/src/constants/industries.ts
@@ -9,4 +9,34 @@ export const Industries: AutoCompleteOption[] = [{
 }, {
     label: 'Biotech/Pharmaceuticals',
     value: 'biotech'
+}, {
+    label: 'Construction, Manufacturing, Utilities/Energy, Transportation',
+    value: 'construction'
+}, {
+    label: 'Education',
+    value: 'education'
+}, {
+    label: 'Financial Services (Including Insurance)',
+    value: 'financial'
+}, {
+    label: 'Healthcare',
+    value: 'healthcare'
+}, {
+    label: 'Information Technology',
+    value: 'it'
+}, {
+    label: 'Legal Services',
+    value: 'legal'
+}, {
+    label: 'Marketing/Media',
+    value: 'marketing'
+}, {
+    label: 'Nonprofit',
+    value: 'nonprofit'
+}, {
+    label: 'Professional Services',
+    value: 'professional'
+}, {
+    label: 'Public Sector',
+    value: 'public'
 }]
