441df473a29d0c1f26c07284656e7a0c048a35c7
Justin Chen <ch3njus@gmail.com>
2023-03-16 23:02:54 -0400

    style: isort bwwc.py

--- backend/backend/apis/bwwc.py
+++ backend/backend/apis/bwwc.py
@@ -6,10 +6,10 @@ sys.path.append("secretshare")
 import json
 
 from django.contrib import admin
-from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.http import (HttpRequest, HttpResponse, HttpResponseBadRequest,
+                         JsonResponse)
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-
 from secretshare.mpce import MPCEngine
 
 engine = MPCEngine()
