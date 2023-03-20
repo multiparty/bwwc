766d47102ee80385771bfde81899206a339eec5c
Greg Frasco <frascog17@gmail.com>
2023-03-20 13:17:47 -0400

    feat: add session context and login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage } from '@pages/home';
 import { CreatePage } from '@pages/create';
 import { ManagePage } from '@pages/manage';
-import { Layout } from '@layouts/layout';
+import { LoginPage } from '@pages/login';
+import { SessionProvider } from '@context/session.context';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/manage" element={<ManagePage />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/manage" element={<ManagePage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
