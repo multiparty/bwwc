3a479db26fe4c1bf28bf2656a02e7d98557f67d2
Greg Frasco <frascog17@gmail.com>
2023-03-23 12:36:37 -0400

    feat: add production domains

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,9 @@ ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://mpc.sail.codes",
+    "https://training.100talent.org",
+    "https://100talent.org"
     # Add other origins if needed
 ]
 
