3aa742df9ef80144f33b88cf27602584a3696f3b
Greg Frasco <frascog17@gmail.com>
2023-03-23 14:22:06 -0400

    fix: & in SAIL

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -46,7 +46,7 @@ export const Footer = () => {
                     Software Applications & Innovation Lab
                   </Typography>
                   <Typography variant="subtitle1" color="gray">
-                    The Software Applications & Innovation Lab (SAIL) is the premier professional research, software engineering, and consulting lab within the Hariri Institute for
+                    The Software & Applications Innovation Lab (SAIL) is the premier professional research, software engineering, and consulting lab within the Hariri Institute for
                     Computing at Boston University.
                   </Typography>
                   <Link href="https://sail.bu.edu" target="_blank">
