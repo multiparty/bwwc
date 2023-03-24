17076f3b2b47f2cd9a87a2c189986b4622cfd2eb
Greg Frasco <frascog17@gmail.com>
2023-03-24 16:25:11 -0400

    feat: add training env banner

--- client/src/layouts/layout.tsx
+++ client/src/layouts/layout.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Box, Container } from '@mui/material';
 import { Header, HeaderProps } from '@components/header';
 import { Footer } from '@components/footer';
+import { TrainingBanner } from '@components/training-banner';
 
 export interface LayoutProps extends HeaderProps {
   children: React.ReactNode;
@@ -12,6 +13,7 @@ export const Layout: FC<LayoutProps> = ({ children, maxWidth, ...headerProps }) 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
       <Header {...headerProps} />
+      <TrainingBanner />
       <Box sx={{ flexGrow: 1, minHeight: '95vh' }}>
         <Container maxWidth={maxWidth || 'xl'} sx={{ mt: 5 }}>
           {children}
