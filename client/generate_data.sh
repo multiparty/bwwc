ead029eedddac5b3f563dc77a7a44db43ecd9d25
Greg Frasco <frascog17@gmail.com>
2023-04-04 14:55:51 -0400

    feat: add logging to data json generation

--- client/generate_data.sh
+++ client/generate_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Generate JSON file
+echo "Generating data.json file"
 cat > /usr/share/nginx/html/data.json <<EOF
 {
 EOF
@@ -10,11 +11,14 @@ while IFS='=' read -r key value; do
   if [[ "$key" == VITE_* ]]; then
     name=$(echo "$key")
     echo "  \"$name\": \"$value\"," >> /usr/share/nginx/html/data.json
+    echo "Adding $name to data.json"
   fi
 done < <(env)
 
 # Complete JSON file
+echo "Adding timestamp to data.json"
 cat >> /usr/share/nginx/html/data.json <<EOF
   "timestamp": $(date +%s)
 }
 EOF
+echo "Done generating data.json file"
