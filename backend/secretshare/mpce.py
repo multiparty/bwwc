68f91feea77a4f815dac855881721d9454cf1c0d
Justin Chen <ch3njus@gmail.com>
2023-04-07 11:44:31 -0400

    feat: add password to redis

--- backend/secretshare/mpce.py
+++ backend/secretshare/mpce.py
@@ -39,10 +39,12 @@ class MPCEngine(object):
         if config_prime and is_prime_miller_rabin(config_prime):
             self.prime = config_prime
 
-        # Connect to Redis and MongoDB
         self.redis_host = os.environ.get("REDIS_HOST", "redis")
-        self.redis_client = redis.Redis(host=self.redis_host, port=6379, db=0)
-        self.redis_client.config_set('readonly', 'no')
+        redis_connection = redis.StrictRedis(host=self.redis_host, port=6379, password=os.getenv("REDIS_PW"), decode_responses=True)
+        redis_connection.config_set('replica-read-only', 'no')
+
+        # self.redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+        # self.redis_client.config_set('requirepass', 'your_password_here')
         self.mongo_host = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
         self.mongo_client = pymongo.MongoClient(self.mongo_host)
         self.mongo_db = self.mongo_client["mpc_database"]
