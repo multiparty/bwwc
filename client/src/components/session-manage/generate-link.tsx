964531714eee90ecb459fb1f8efc3431257ea4a3
Harunobu Ishii <harunobuishii547@gmail.com>
2023-04-12 15:18:41 -0400

    Copy button added

--- client/src/components/session-manage/generate-link.tsx
+++ client/src/components/session-manage/generate-link.tsx
@@ -1,12 +1,36 @@
 import { FC, useState } from 'react';
 import { Formik, Form, FormikHelpers } from 'formik';
-import { Grid, Box, Typography, Stack, Divider } from '@mui/material';
+import { Card, CardContent, Grid, Box, Typography, Stack, Divider, Button, Snackbar } from '@mui/material';
 import * as Yup from 'yup';
 import { TextInput } from '@components/forms/text-input';
 import { SubmitButton } from '@components/forms/submit-button';
 import { useApi } from '@services/api';
 import { AppState } from '@utils/data-format';
 import { useSelector } from 'react-redux';
+import CopyAllIcon from '@mui/icons-material/CopyAll';
+
+type CopyToClipboardButtonProps = {
+  links: string[];
+};
+
+function CopyToClipboardButton(props: CopyToClipboardButtonProps) {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(true);
+    const linksToCopy = props.links.join('; ');
+    navigator.clipboard.writeText(linksToCopy);
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick}>
+        <CopyAllIcon color="action" />
+      </Button>
+
+      <Snackbar open={open} onClose={() => setOpen(false)} autoHideDuration={2000} message="Copied to clipboard" />
+    </>
+  );
+}
 
 export const LinkGenerator: FC = () => {
   const [generatedLinks, setGeneratedLinks] = useState<string[]>([]);
@@ -30,49 +54,72 @@ export const LinkGenerator: FC = () => {
   };
 
   return (
-    <Stack spacing={2} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+    <Stack spacing={3} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
       <Box style={{ width: '50%' }}>
         <Formik validationSchema={validationSchema} initialValues={{ count: 0 }} onSubmit={handleSubmit}>
           <Form>
-            <Stack spacing={2}>
+            <Stack spacing={0}>
               <Typography variant="h5">Add Participants</Typography>
+              <Stack spacing={2}>
               <Typography variant="subtitle1">Generate more URLs for new participants.</Typography>
+              
               <TextInput fullWidth name="count" label="New participants" type="number" />
               <SubmitButton type="submit" variant="contained" fullWidth>
                 Submit
               </SubmitButton>
+              </Stack>
             </Stack>
           </Form>
         </Formik>
       </Box>
       <Divider />
-      <Stack spacing={2}>
+
+      {generatedLinks.length != 0 &&(      <Stack spacing={0}>
         <Typography variant="h5">Generated Links</Typography>
         <Typography variant="subtitle1">The following links can be sent to participants to join the session.</Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-          <Stack spacing={2}>
-            {generatedLinks.map((link, index) => (
-              <Typography key={index} variant="subtitle1">
-                {link}
-              </Typography>
-            ))}
-          </Stack>
+          <Card>
+            <Grid container direction="row" alignItems="center" sx={{ justifyContent: 'flex-end' }}>
+              <Grid item sx={{ marginLeft: 'auto' }}>
+                 <CopyToClipboardButton links={generatedLinks} />
+              </Grid>
+            </Grid>
+            <CardContent>
+              <Stack spacing={0}>
+                {generatedLinks.map((link, index) => (
+                  <Typography key={index} variant="subtitle1">
+                    {link}
+                  </Typography>
+                ))}
+              </Stack>
+            </CardContent>
+          </Card>
         </Box>
-      </Stack>
+      </Stack>)}
       <Divider />
-      <Stack spacing={2}>
+
+      {existingLinks.length != 0 && (<Stack spacing={0}>
         <Typography variant="h5">Existing Participant Links</Typography>
         <Typography variant="subtitle1">The following links can be sent to participants to join the session.</Typography>
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-          <Stack spacing={2}>
-            {existingLinks.map((link, index) => (
-              <Typography key={index} variant="subtitle1">
-                {link}
-              </Typography>
-            ))}
-          </Stack>
+          <Card>
+            <Grid container direction="row" alignItems="center" sx={{ justifyContent: 'flex-end' }}>
+              <Grid item sx={{ marginLeft: 'auto' }}>
+                <CopyToClipboardButton links={existingLinks} />
+              </Grid>
+            </Grid>
+            <CardContent>
+              <Stack spacing={0}>
+                {existingLinks.map((link, index) => (
+                  <Typography key={index} variant="subtitle1">
+                    {link}
+                  </Typography>
+                ))}
+              </Stack>
+            </CardContent>
+          </Card>
         </Box>
-      </Stack>
+      </Stack>)}
     </Stack>
   );
 };
