533541fa6cc4806f415597abe08654434eba66a8
Harunobu Ishii <harunobuishii547@gmail.com>
2023-04-18 11:24:40 -0400

    Cypress installed

--- client/cypress/e2e/submit-input.cy.js
+++ client/cypress/e2e/submit-input.cy.js
@@ -1,15 +1,15 @@
 import { dataGenerator } from '../support/custom/generate-input';
 import { startSession } from '../support/custom/session-create';
-describe('deepEqual', () => {
+
+describe('data generation', () => {
 it('create and encrypt user input', () => {
-    // Create csv
+  // create csv
   const input = dataGenerator();
   console.log(input);
-    // input id, code, industry, size
-    // Drop the csv
-    // Click box
-    // Click submit
-
-    // console.log(startSession());
+  // input id, code, industry, size
+  // drop the csv
+  // click box
+  // click submit
+  // encrypt
   });
 });
