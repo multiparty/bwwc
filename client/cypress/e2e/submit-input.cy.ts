87f8a591a771da3afd37fea2c0c72995f6789d36
Harunobu Ishii <harunobuishii547@gmail.com>
2023-04-20 16:53:59 -0400

    Xlsx file generator done

--- client/cypress/e2e/submit-input.cy.ts
+++ client/cypress/e2e/submit-input.cy.ts
@@ -1,26 +1,27 @@
-import { dataGenerator,dataObjectToXlsx } from '../support/custom/generate-input';
+import { dataGenerator, dataObjectToXlsx } from '../support/custom/generate-input';
 import 'cypress-file-upload';
 
 describe('data generation', () => {
   beforeEach(() => {
-    cy.visit("http://127.0.0.1:5173/");
+    cy.visit('http://127.0.0.1:5173/');
   });
 
-it('create and encrypt user input', () => {
+  it('create and encrypt user input', () => {
+    const dataObj = dataGenerator();
+    const fileName = 'testData.xlsx';
+    const xlsxData = dataObjectToXlsx(dataObj, fileName);
+    cy.get('[data-cy="dropzone"]', { timeout: 10000 })
+      .attachFile({
+        fileContent: new Blob([xlsxData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }),
+        fileName: fileName,
+        mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        encoding: 'utf8',
+        lastModified: new Date().getTime()
+      })
+      .trigger('change');
 
-  const dataObj = dataGenerator()
-  const fileName = 'testData.xlsx';
-  const xlsxData = dataObjectToXlsx(dataObj, fileName);
-  cy.get('[data-cy="dropzone"]', { timeout: 10000 })
-  .attachFile({ 
-                fileContent: new Blob([xlsxData], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }),
-                fileName: fileName,
-                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                encoding: 'utf8', 
-                lastModified: new Date().getTime() }).trigger('change');
-
-  // input id, code, industry, size
-  // click box
-  // click submit
+    // input id, code, industry, size
+    // click box
+    // click submit
   });
 });
--- client/cypress/support/custom/generate-input.ts
+++ client/cypress/support/custom/generate-input.ts
@@ -16,6 +16,10 @@ interface PositionMap {
   [key: string]: EthnicityMap;
 }
 
+type KeyedObject = {
+  [key: string]: any;
+};
+
 type ObjectArray = Array<{ [key: string]: any }>;
 
 const dataTypes = ['numberOfEmployees', 'wages', 'performance', 'lengthOfService', 'totalEmployees'];
@@ -28,7 +32,7 @@ const sheet_name_Mapping: { [key in keyof DataFormat]: string } = {
   totalEmployees: 'Totals Check'
 };
 
-function genRandomInt(max:number){
+function genRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
 
@@ -40,13 +44,12 @@ function gridDataToJSON(rows: TableRow[]) {
   });
   return res;
 }
-
+type ExtendedDataFormat = DataFormat & { [key: string]: PositionMap };
 
 export function dataGenerator() {
-  let counter = {F:0, M:0, NB:0, all:0}
-  
-  return dataTypes.reduce((acc: DataFormat, value: string) => {
-    console.log(value)
+  let counter = { F: 0, M: 0, NB: 0, all: 0 };
+
+  return dataTypes.reduce((acc: ExtendedDataFormat, value: string) => {
     const pos: PositionMap = Object.keys(Positions).reduce((acc, p) => {
       const position = Positions[p as keyof typeof Positions];
 
@@ -55,11 +58,11 @@ export function dataGenerator() {
 
         const gender: GenderMap = Object.keys(Gender).reduce((acc, g) => {
           const gen = Gender[g as keyof typeof Gender];
-          const randVal=genRandomInt(10000);
-          acc[gen] = randVal
-          if(value=='numberOfEmployees'){
-            counter[gen]+= randVal
-            counter['all']+= randVal
+          const randVal = genRandomInt(10000);
+          acc[gen] = randVal;
+          if (value == 'numberOfEmployees') {
+            counter[gen] += randVal;
+            counter['all'] += randVal;
           }
           return acc;
         }, {} as GenderMap);
@@ -71,56 +74,52 @@ export function dataGenerator() {
       acc[position] = ethnicity;
       return acc;
     }, {} as PositionMap);
-    if(value=='totalEmployees'){
-      acc['totalEmployees']=counter;
-    }else{
-      console.log(value)
+    if (value == 'totalEmployees') {
+      acc['totalEmployees'] = counter;
+    } else {
       acc[value] = pos;
     }
     return acc;
-  }, {} as DataFormat);
+  }, {} as ExtendedDataFormat);
 }
 
 export function dataObjectToXlsx(dataObjects: DataFormat, filename: string): ArrayBuffer {
   const wb = XLSX.utils.book_new();
   // Adding an empty, first sheet
-  const empty_sheet: any[] = []
-  let ws = XLSX.utils.aoa_to_sheet(empty_sheet)
-  const first_sheet = "Enter Data →"
+  const empty_sheet: any[] = [];
+  let ws = XLSX.utils.aoa_to_sheet(empty_sheet);
+  const first_sheet = 'Enter Data →';
   XLSX.utils.book_append_sheet(wb, ws, first_sheet);
   // Adding sheets for respective statistics
-  Object.keys(dataObjects).forEach((dataObj , index)=>{
-
-    if(dataObj!=='totalEmployees'){
-    const curr_data = dataObjects[dataObj as keyof DataFormat]
-    const sheet_name = sheet_name_Mapping[dataObj as keyof DataFormat]
-    const opt = { origin: { r: 5, c: 0 } };
-    const rows = convertToRows(curr_data as TableData);
-    const jsonData = gridDataToJSON(rows);
-    console.log(jsonData)
-    ws = XLSX.utils.json_to_sheet(jsonData, opt);
-    XLSX.utils.book_append_sheet(wb, ws, sheet_name);
-  }
-})
+  Object.keys(dataObjects).forEach((dataObj, index) => {
+    if (dataObj !== 'totalEmployees') {
+      const curr_data = dataObjects[dataObj as keyof DataFormat];
+      const sheet_name = sheet_name_Mapping[dataObj as keyof DataFormat];
+      const opt = { origin: { r: 5, c: 0 } };
+      const rows = convertToRows(curr_data as TableData);
+      const jsonData = gridDataToJSON(rows);
+      ws = XLSX.utils.json_to_sheet(jsonData, opt);
+      XLSX.utils.book_append_sheet(wb, ws, sheet_name);
+    }
+  });
 
   // Adding an empty sheet
-  ws = XLSX.utils.aoa_to_sheet(empty_sheet)
-  const check_sheet = "Check Data →"
+  ws = XLSX.utils.aoa_to_sheet(empty_sheet);
+  const check_sheet = 'Check Data →';
   XLSX.utils.book_append_sheet(wb, ws, check_sheet);
 
   // Adding a total check sheet
-  const totalEmployees = dataObjects['totalEmployees']
+  const totalEmployees = dataObjects['totalEmployees'];
   const totalEmployeesArray = [
-  Object.entries(totalEmployees).reduce((acc, [key, value]) => {
-    acc[key] = value;
-    return acc;
-  }, {}),
-];
-  console.log(totalEmployeesArray)
+    Object.entries(totalEmployees).reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {} as KeyedObject)
+  ];
+  console.log(totalEmployeesArray);
   const origin = { r: 4, c: 1 };
   XLSX.utils.sheet_add_json(ws, totalEmployeesArray, { origin });
-
-  const last_sheet = "Totals Check";
+  const last_sheet = 'Totals Check';
   XLSX.utils.book_append_sheet(wb, ws, last_sheet);
   return XLSX.writeFile(wb, filename);
 }
