4c083e028a7041e6e2b51d3ab12f25819e848190
Justin Chen <ch3njus@gmail.com>
2023-04-27 12:05:10 -0400

    fix: progress bar

--- client/src/components/session-decrypt/decrypt-input.tsx
+++ client/src/components/session-decrypt/decrypt-input.tsx
@@ -84,9 +84,13 @@ export const DecryptInputForm: FC<CompanyInputFormProps> = (props) => {
         const fileContent = event.target?.result as string;
         const privateCryptoKey = await importPemPrivateKey(fileContent);
         const {data, total_cells} = await getSubmissions(sessionId, token);
-        const decodedTable = await secretSharesToTable(data, privateCryptoKey, bigPrime, reduce, setProgress);
+
+        const recordProgress = (progress: number) => {
+          setProgress(progress / total_cells * 100);
+        };
+
+        const decodedTable = await secretSharesToTable(data, privateCryptoKey, bigPrime, reduce, recordProgress);
         dispatch(setDecodedTable(decodedTable));
-        console.log(`total_cells: ${total_cells}`)
       }
     };
 
