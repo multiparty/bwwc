c965c0352a3ad265f8ee2ec0d6e6bd96872b0abf
Justin Chen <ch3njus@gmail.com>
2023-05-10 17:08:46 -0400

    refactor: remove unused settings variabls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,13 +121,6 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
