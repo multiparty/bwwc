180e46ea04cd2781c4ba755da3bc773eabad60b3
Greg Frasco <frascog17@gmail.com>
2023-05-10 16:09:41 -0400

    feat: mongo

--- client/src/pages/create.tsx
+++ client/src/pages/create.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { SessionCreateForm } from '@components/session-create/session-create';
 import { Layout } from '@layouts/layout';
+import { NavBar } from '@components/navbar/navbar';
 
 export const CreatePage: FC = () => {
   return (
     <Layout title="Trusted Party" subtitle="Secure Session Creator" maxWidth="lg">
+      <NavBar />
       <SessionCreateForm />
     </Layout>
   );
