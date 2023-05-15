8cbd1319ea6da835c3524a887b78d7cd204b1240
Harunobu Ishii <harunobuishii547@gmail.com>
2023-05-15 12:58:36 -0400

    Navbar modified

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
-import { Stack, Toolbar, IconButton, Drawer,Menu, MenuItem } from '@mui/material';
+import { Stack, Toolbar, IconButton, Drawer, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
@@ -12,7 +12,6 @@ type nameProp = {
 };
 
 export const NavBar = () => {
-  
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -38,12 +37,11 @@ export const NavBar = () => {
 
     return (
       <Box
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-start',
-        alignItems: 'center'
-      }}
-      
+        style={{
+          display: 'flex',
+          justifyContent: 'flex-start',
+          alignItems: 'center'
+        }}
       >
         <Button
           variant="outlined"
@@ -63,36 +61,36 @@ export const NavBar = () => {
 
   return (
     <Box sx={{ display: 'flex', width: '100%' }}>
-    <AppBar
-      position="static"
-      sx={{
-        height: '45px',
-        width: '100%',
-        backgroundColor: 'white',
-        '& .MuiDrawer-paper': {
-          height: '60px',
+      <AppBar
+        position="static"
+        sx={{
+          height: '45px',
           width: '100%',
-          justifyContent: 'space-between'
-        }
-      }}
-    >
-      <Toolbar variant="dense">
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleMenuClick}>
-          <MenuIcon sx={{color:'grey'}}/>
-        </IconButton>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}><NavBarButtons name="Home" /></MenuItem>
-          <MenuItem onClick={handleClose}><NavBarButtons name="Create" /></MenuItem>
-          <MenuItem onClick={handleClose}><NavBarButtons name="Manage" /></MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
-  </Box>
-);
+          backgroundColor: 'white',
+          '& .MuiDrawer-paper': {
+            height: '60px',
+            width: '100%',
+            justifyContent: 'space-between'
+          }
+        }}
+      >
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={handleMenuClick}>
+            <MenuIcon sx={{ color: 'grey' }} />
+          </IconButton>
+          <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+            <MenuItem onClick={handleClose}>
+              <NavBarButtons name="Home" />
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <NavBarButtons name="Create" />
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <NavBarButtons name="Manage" />
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
