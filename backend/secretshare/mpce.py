78212a0b6a1941ac5a08ae14bb4815925742f92f
asadeg02 <32320836+asadeg02@users.noreply.github.com>
2025-04-07 14:05:43 -0400

    fix: URL-encode MongoDB credentials to prevent InvalidURI errors

--- backend/secretshare/mpce.py
+++ backend/secretshare/mpce.py
@@ -3,6 +3,7 @@ import sys
 import uuid
 import numbers
 import logging
+import urllib.parse
 from typing import Any, Dict, List, Optional, Tuple, Union, Callable
 from itertools import groupby
 from operator import itemgetter
@@ -35,8 +36,8 @@ class MPCEngine(object):
 
         self.mongo_host = os.environ.get("MONGO_HOST")
         self.mongo_port = os.environ.get("MONGO_PORT")
-        self.mongo_user = os.environ.get("MONGO_USER", "bwwc")
-        self.mongo_password = os.environ.get("MONGO_PASSWORD")
+        self.mongo_user = urllib.parse.quote_plus(os.environ.get("MONGO_USER", "bwwc"))
+        self.mongo_password = urllib.parse.quote_plus(os.environ.get("MONGO_PASSWORD")
         self.mongo_db = os.environ.get("MONGO_DB", "bwwc") 
         self.mongo_params = os.environ.get("MONGO_PARAMS", "") 
         mongo_uri = f"mongodb://{self.mongo_user}:{self.mongo_password}@{self.mongo_host}:{self.mongo_port}/?{self.mongo_params}"
